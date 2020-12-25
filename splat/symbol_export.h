90aa6ad718e45dc75cf348d99fcd096145474a8a
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-25 16:40:16 +0200

    symbol_export

---
+++ splat/symbol_export.h
@@ -0,0 +1,19 @@
+// splat
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+
+// Shared library interface
+#if !defined(SYMBOL_IMPORT)
+#   if defined(_MSC_VER)
+#       define SYMBOL_EXPORT __declspec(dllexport)
+#       define SYMBOL_IMPORT __declspec(dllimport)
+#   else
+#       define SYMBOL_EXPORT __attribute__((__visibility__("default")))
+#       define SYMBOL_IMPORT
+#   endif
+#endif
