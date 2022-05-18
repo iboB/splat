41cbc560fff7d7c7dc97e31bf1fc1e4578c75e1e
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-05-18 17:44:49 +0300

    Macros do disable warnings. Closes #1

---
+++ splat/warnings.h
@@ -0,0 +1,38 @@
+// splat
+// Copyright (c) 2020-2022 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+
+// macros to enable and disable warning
+
+#if defined(_MSC_VER)
+#   define PRAGMA_WARNING_PUSH __pragma(warning(push))
+#   define PRAGMA_WARNING_POP  __pragma(warning(pop))
+#   define DISABLE_MSVC_WARNING(w) __pragma(warning(disable : w))
+#   define DISABLE_GCC_ONLY_WARNING(w)
+#   define DISABLE_CLANG_ONLY_WARNING(w)
+#   define DISABLE_GCC_AND_CLANG_WARNING(w)
+#elif defined(__GNUC__) // also defined by clang
+#   define PRAGMA_WARNING_PUSH _Pragma("GCC diagnostic push")
+#   define PRAGMA_WARNING_POP  _Pragma("GCC diagnostic pop")
+#   define I_SPLAT_PRAGMA(x) _Pragma(#x)
+#   define DISABLE_GCC_AND_CLANG_WARNING(w) I_SPLAT_PRAGMA(GCC diagnostic ignored w)
+#   if defined(__clang__)
+#       define DISABLE_GCC_ONLY_WARNING(w)
+#       define DISABLE_CLANG_ONLY_WARNING(w) I_SPLAT_PRAGMA(GCC diagnostic ignored w)
+#   else
+#       define DISABLE_GCC_ONLY_WARNING(w) I_SPLAT_PRAGMA(GCC diagnostic ignored w)
+#       define DISABLE_CLANG_ONLY_WARNING(w)
+#   endif
+#else
+#   define PRAGMA_WARNING_PUSH
+#   define PRAGMA_WARNING_POP
+#   define DISABLE_MSVC_WARNING(w)
+#   define DISABLE_GCC_ONLY_WARNING(w)
+#   define DISABLE_CLANG_ONLY_WARNING(w)
+#   define DISABLE_GCC_AND_CLANG_WARNING(w)
+#endif
