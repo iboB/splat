1601d41fcc2d6cdd9da532bc7731128a8b505080
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-02-07 09:54:09 +0200

    Renamed force_inline.h to inline.h. NOINLINE macro

--- splat/inline.h
+++ splat/inline.h
@@ -3,7 +3,7 @@
 //
 #pragma once
 
-// macro to force inlining of functions
+// macro to force inlining (or no-inlining) of functions
 
 #if defined(__GNUC__)
 #   define FORCE_INLINE __attribute__((always_inline))
@@ -11,4 +11,10 @@
 #   define FORCE_INLINE  __forceinline
 #else
 #   define FORCE_INLINE  inline
+#endif
+
+#if defined(_MSC_VER)
+#   define NOINLINE __declspec(noinline)
+#else
+#   define NOINLINE __attribute__((noinline))
 #endif
